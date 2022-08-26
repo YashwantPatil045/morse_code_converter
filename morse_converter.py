a4441e54fe6576c0f5a269c130e27ea9365a0304
Yashwant <yashwantpatil045@gmail.com>
2022-08-26 09:34:11 +0530

    Shortened the code, add some special characters

--- morse_converter.py
+++ morse_converter.py
@@ -1,35 +1,32 @@
 morse_data = {
-    '/': '/',
     'a': '.-', 'b': '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '-.', 'h': '....', 'i': '..',
     'j': '.-', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.', 'q': '--.-', 'r': '.-.',
     's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-', 'y': '-.--', 'z': '--..',
 
     '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..',
-    '9': '----.', '0': '-----'
+    '9': '----.', '0': '-----',
+
+    '.': ".-.-.-", ',': "--..--", '?': "..--..", "'": ".----.", '!': "-.-.--", '(': "-.--.", ')': "-.--.-",
+    '&': ".-...", ':': "---...", ';': "-.-.-.", '=': "-...-", '+': ".-.-.", '-': "-....-", '_': "..__._", '"': ".-..-.",
+    '$': "...-..-", '@': ".--.-.",
+
+    '/': '/'
 }
-chars = "~!@#$%^&*()_+="
 
 
 def morse_code_converter():
     msg = input("Write your message: ")
-    words = msg.split()
-    sep_msg = ""
-    for word in words:
-        sep_words = [word + "/"]
-        for letters in sep_words:
-            for letter in list(letters):
-                if letter not in chars:
-                    sep_msg += morse_data[letter.lower()] + " "
-                else:
-                    print("Special characters won't be converted into Morse Code. Try again")
-                    return
-
-    print(f"Here is a Morse Code: {sep_msg[:-2]}")
-    if len(words) > 1:
-        print("(Note: Words are separated by '/')")
+    words = list(msg.replace(" ", '/'))
+    code = []
+    for letter in words:
+        try:
+            code.append(morse_data[letter.lower()])
+        except KeyError:
+            return print("Sorry... Some characters can't be converted into Morse Code.")
+    final_msg = " ".join(code)
+    print(f"Here's your message converted into the Morse code:\n\n {final_msg}")
+    if "/" in final_msg:
+        print("\n(Note: Words are separated by '/')")
 
 
 morse_code_converter()
-
-
-
